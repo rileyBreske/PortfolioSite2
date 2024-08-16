23dbd3d254535e97c718afe81433e810fc86ce04
rileyBreske <rilobreske@gmail.com>
2024-08-16 01:26:30 -0400

    added link to EDA pdf

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import styles from "./ProjectsStyles.module.css";
 import mash from "../../assets/mash.png";
 import rise from "../../assets/rise.png";
 import eda from "../../assets/graph.png";
+import edaPDF from "../../assets/Exploratory Data Analysis_ Airline Delays.pdf";
 import fitLift from "../../assets/fitlift.png";
 import ProjectCard from "../../common/ProjectCard";
 
@@ -12,7 +13,9 @@ function Projects() {
       <div className={styles.projectsContainer}>
         <ProjectCard src={mash} link="https://www.mashelite.com/" h3="Mash Elite" p="Wordpress Dev" />
         <ProjectCard src={rise} link="https://www.riseindoorsports.com/" h3="Rise Indoor" p="Wordpress Dev" />
-        <ProjectCard src={eda} link="" h3="Airline EDA" p="R programming" />
+        <a href={edaPDF} target="_blank" rel="noopener noreferrer">
+          <ProjectCard src={eda} h3="Airline EDA" p="R programming" />
+        </a>
         <ProjectCard src={fitLift} link="" h3="Ran. Forest Model" p="R programming" />
       </div>
     </section>
