df6cbe3bc5859a51eb126d3f761f3827abe99bb5
rileyBreske <rilobreske@gmail.com>
2024-08-31 23:32:56 -0400

    project update

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import styles from "./ProjectsStyles.module.css";
 import mash from "../../assets/mash.png";
-import rise from "../../assets/rise.png";
+import profilerV2 from "../../assets/profilerV2.png";
 import eda from "../../assets/eda.png";
 import edaPDF from "../../assets/Exploratory Data Analysis_ Airline Delays.pdf";
 import ranforestPDF from "../../assets/Political Party Classification Using Random Forest.pdf";
@@ -13,7 +13,7 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard src={mash} link="https://www.mashelite.com/" h3="Mash Elite" p="Wordpress Dev" />
-        <ProjectCard src={rise} link="https://www.riseindoorsports.com/" h3="Rise Indoor" p="Wordpress Dev" />
+        <ProjectCard src={profilerV2} link="https://profilerv2.vercel.app/" h3="ProfilerV2" p="Next.js/ Supabase" />
         <a href={edaPDF} target="_blank" rel="noopener noreferrer">
           <ProjectCard src={eda} h3="Airline EDA" p="R programming" />
         </a>
